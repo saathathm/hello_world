87c7c48fbe5c0e8be7a1d309b20ad47077cba54b
saathathm <saaadhath@gmail.com>
2025-02-09 21:43:45 +0530

    edited

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
 
-async function handleRequest(request) {
-  // Get the URL path from the request
-  const url = new URL(request.url);
+    if (url.pathname === "/hello") {
+      return new Response("Hello, World!", { status: 200 });
+    } else if (url.pathname === "/about") {
+      return new Response("About Us Page", { status: 200 });
+    }
 
-  // Routing based on the URL path
-  if (url.pathname === "/about") {
-    return new Response(
-      JSON.stringify({
-        message: "Hi, I'm a Cloudflare Worker! This is the 'About Me' page.",
-        details: {
-          name: "Your Name",
-          profession: "Your Profession",
-          hobbies: ["Coding", "Music", "Gaming"],
-        },
-      }),
-      { headers: { "Content-Type": "application/json" } }
-    );
-  } else if (url.pathname === "/") {
-    // Default Hello World page
-    return new Response("Hello, World!", {
-      headers: { "Content-Type": "text/plain" },
-    });
-  } else {
-    // Handle 404 - Page not found
-    return new Response("404 Not Found", { status: 404 });
-  }
-}
+    return new Response("Not Found", { status: 404 });
+  },
+};
